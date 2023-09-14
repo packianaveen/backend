9ed2877bd650126585834305bcb29b87e8ae16f1
packianaveen <packianaveen123@gmail.com>
2023-09-15 04:28:08 +0530

    live push

--- Routes/trendad.js
+++ Routes/trendad.js
@@ -8,7 +8,7 @@ let trendadSchema = require("../Models/Trendad");
 
 const storage = multer.diskStorage({
   destination: (req, file, cd) => {
-    cd(null, "images");
+    cd(null, "Images");
   },
   filename: (req, file, cd) => {
     cd(null, Date.now() + "-" + file.originalname);
